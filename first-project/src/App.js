2e008dcdfd1c5cd8785d61603a5139a7b90f4527
Alejandro Such Berenguer <alejandro.such@gmail.com>
2018-11-29 00:28:43 +0100

    Add section 3 code examples

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -1,27 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    people: [
+      { name: 'Alex', age: 37, other: null },
+      { name: 'Sandra', age: 36, other: null },
+      { name: 'Martina', age: 4, other: null },
+      { name: 'Mario', age: 0, other: 'Repta muy bien' }
+    ]
+  };
+
+  switchNameHandler = name => {
+    this.setState({
+      people: this.state.people.map(it => (it.name === name ? Object.assign({}, it, { name: 'Alejandro' }) : it))
+    });
+  };
+
+  nameChangedHandler = ({ oldName, newName }) => {
+    this.setState({
+      people: this.state.people.map(it => (it.name === oldName ? Object.assign({}, it, { name: newName }) : it))
+    });
+  };
+
   render() {
+    const style = {
+      backGroundColor: '#DEDEDE',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+        <h1>Hi, I'm a react app</h1>
+        <p>This is really working!</p>
+        <button style={style} onClick={this.switchNameHandler}>
+          Switch name
+        </button>
+        {this.state.people.map((person, index) => (
+          <Person
+            key={index}
+            name={person.name}
+            age={person.age}
+            click={this.switchNameHandler}
+            changed={this.nameChangedHandler}
           >
-            Learn React
-          </a>
-        </header>
+            {person.other}
+          </Person>
+        ))}
       </div>
     );
+
+    // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, `Hi, I'm a react app`));
   }
 }
 
---
+++ first-project/src/Person/Person.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Person.css';
+
+const paintChildren = children => (children ? <p>{children}</p> : null);
+
+const person = ({ name, age, children, click, changed }) => (
+  <div className="Person">
+    <p onClick={() => click(name)}>
+      I'm {name} and I am {age} years old
+    </p>
+    {paintChildren(children)}
+    <input type="text" value={name} onInput={event => changed({ oldName: name, newName: event.target.value })} />
+  </div>
+);
+export default person;
