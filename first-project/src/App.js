ab491037be070683007b3495f50a877f28a0c934
Alejandro Such Berenguer <alejandro.such@gmail.com>
2018-11-30 23:47:41 +0100

    working with lists exercises

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -5,11 +5,12 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     people: [
-      { name: 'Alex', age: 37, other: null },
-      { name: 'Sandra', age: 36, other: null },
-      { name: 'Martina', age: 4, other: null },
-      { name: 'Mario', age: 0, other: 'Repta muy bien' }
-    ]
+      { id: '001', name: 'Alex', age: 37, other: null },
+      { id: '002', name: 'Sandra', age: 36, other: null },
+      { id: '003', name: 'Martina', age: 4, other: null },
+      { id: '004', name: 'Mario', age: 0, other: 'Repta muy bien' }
+    ],
+    showPeople: true
   };
 
   switchNameHandler = name => {
@@ -18,11 +19,45 @@ class App extends Component {
     });
   };
 
-  nameChangedHandler = ({ oldName, newName }) => {
+  deletePersonHandler = index => {
     this.setState({
-      people: this.state.people.map(it => (it.name === oldName ? Object.assign({}, it, { name: newName }) : it))
+      people: this.state.people.filter((_, idx) => index !== idx)
     });
   };
+
+  nameChangedHandler = ({ newName }, index) => {
+    this.setState({
+      people: this.state.people.map((item, idx) => {
+        if (idx !== index) {
+          return item;
+        }
+
+        return Object.assign({}, item, { name: newName });
+      })
+    });
+  };
+
+  togglePeopleHandler = () => {
+    this.setState({ showPeople: !this.state.showPeople });
+  };
+
+  peopleList() {
+    return (
+      <>
+        {this.state.people.map((person, index) => (
+          <Person
+            key={person.id}
+            name={person.name}
+            age={person.age}
+            click={() => this.deletePersonHandler(index)}
+            changed={event => this.nameChangedHandler(event, index)}
+          >
+            {person.other}
+          </Person>
+        ))}
+      </>
+    );
+  }
 
   render() {
     const style = {
@@ -33,6 +68,11 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let people = null;
+    if (this.state.showPeople) {
+      people = this.peopleList();
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a react app</h1>
@@ -40,17 +80,10 @@ class App extends Component {
         <button style={style} onClick={this.switchNameHandler}>
           Switch name
         </button>
-        {this.state.people.map((person, index) => (
-          <Person
-            key={index}
-            name={person.name}
-            age={person.age}
-            click={this.switchNameHandler}
-            changed={this.nameChangedHandler}
-          >
-            {person.other}
-          </Person>
-        ))}
+        <button style={style} onClick={this.togglePeopleHandler}>
+          Hide all
+        </button>
+        {people}
       </div>
     );
 
--- first-project/src/Person/Person.js
+++ first-project/src/Person/Person.js
@@ -9,7 +9,7 @@ const person = ({ name, age, children, click, changed }) => (
       I'm {name} and I am {age} years old
     </p>
     {paintChildren(children)}
-    <input type="text" value={name} onInput={event => changed({ oldName: name, newName: event.target.value })} />
+    <input type="text" value={name} onChange={event => changed({ oldName: name, newName: event.target.value })} />
   </div>
 );
 export default person;
